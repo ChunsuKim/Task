167ba9ca19fc69209f47bcc261b30b2f3de51328
ChunsuKim <chunsu0816@gmail.com>
2019-04-03 10:20:21 +0900

    Assignment 20190401 typecasting

---
+++ Asignment190401.playground/Contents.swift
@@ -0,0 +1,19 @@
+/*
+[ 과제 ]
+1.
+func addTwoValues(a: Int, b: Int) -> Int {
+    return a + b
+}
+let task1: Any = addTwoValues(a: 2, b: 3)
+위와 같이 task1 이라는 변수가 있을 때
+task1 + task1 의 결과가 제대로 출력되도록 할 것
+ */
+
+func addTwoValues(a: Int, b: Int) -> Int {
+    return a + b
+}
+
+let task1: Any = addTwoValues(a: 2, b: 3)
+var typecastingTask1: Int = task1 as! Int
+print(typecastingTask1 + typecastingTask1)
+
