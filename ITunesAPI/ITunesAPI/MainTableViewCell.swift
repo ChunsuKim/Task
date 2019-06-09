45c48556ad591a98dc71f18bc828d70d38a2f9ff
chunsu Kim <chunsu0816@gmail.com>
2019-06-09 17:34:46 +0900

    ITunesAPI

---
+++ ITunesAPI/ITunesAPI/MainTableViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  MainTableViewCell.swift
+//  ITunesAPI
+//
+//  Created by Chunsu Kim on 09/06/2019.
+//  Copyright © 2019 Chunsu Kim. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    
+    static let identifier = "iTunesTableViewCell"
+    let musicImageView = UIImageView()
+    let musicNameLabel = UILabel()
+    let artistNameLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configure()
+        autoLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    private func configure() {
+        musicNameLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        contentView.addSubview(musicImageView)
+        contentView.addSubview(musicNameLabel)
+        contentView.addSubview(artistNameLabel)
+    }
+    
+    private func autoLayout() {
+        musicImageView.translatesAutoresizingMaskIntoConstraints = false
+        musicImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        musicImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        musicImageView.widthAnchor.constraint(equalTo: musicImageView.heightAnchor).isActive = true
+        musicImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+        musicNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        musicNameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
+        musicNameLabel.leadingAnchor.constraint(equalTo: musicImageView.trailingAnchor, constant: 10).isActive = true
+        musicNameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        
+        artistNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        artistNameLabel.topAnchor.constraint(equalTo: musicNameLabel.bottomAnchor).isActive = true
+        artistNameLabel.leadingAnchor.constraint(equalTo: musicImageView.trailingAnchor, constant: 10).isActive = true
+        artistNameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        artistNameLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20).isActive = true
+        artistNameLabel.heightAnchor.constraint(equalTo: musicNameLabel.heightAnchor).isActive = true
+    }
+
+}
---
+++ ITunesAPI/ITunesAPI/ViewController.swift
@@ -0,0 +1,111 @@
+//
+//  MainTableViewController.swift
+//  ITunesAPI
+//
+//  Created by Chunsu Kim on 08/06/2019.
+//  Copyright © 2019 Chunsu Kim. All rights reserved.
+//
+
+import Alamofire
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let iTunesSearchTableView = UITableView()
+    private var url = "https://itunes.apple.com/search?media=music&entity=song&term="
+    var urlMusicData: [MusicData] = []
+    let searchController = UISearchController(searchResultsController: nil)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configure()
+        autoLayout()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        getData()
+    }
+    
+    private func configure() {
+        iTunesSearchTableView.register(MainTableViewCell.self, forCellReuseIdentifier: MainTableViewCell.identifier)
+        iTunesSearchTableView.dataSource = self
+        iTunesSearchTableView.rowHeight = 80
+        
+        searchController.searchBar.delegate = self
+        searchController.searchBar.sizeToFit()
+        navigationItem.searchController = searchController
+        
+        searchController.searchBar.placeholder = "Search Music"
+        searchController.obscuresBackgroundDuringPresentation = false
+        
+        view.addSubview(iTunesSearchTableView)
+    }
+    
+    private func autoLayout() {
+        iTunesSearchTableView.translatesAutoresizingMaskIntoConstraints = false
+        iTunesSearchTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        iTunesSearchTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        iTunesSearchTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        iTunesSearchTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+    private func getData() {
+        let req = AF.request(url)
+        req
+            .validate()
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    do {
+                        let musicData = try JSONDecoder().decode(Music.self, from: data)
+                        self.urlMusicData = musicData.result
+                        DispatchQueue.main.async {
+                            self.iTunesSearchTableView.reloadData()
+                        }
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+        }
+    }
+}
+
+    // MARK: - Table view data source
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return urlMusicData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MainTableViewCell.identifier, for: indexPath) as! MainTableViewCell
+        let urlString = urlMusicData[indexPath.row].artworkUrl100
+        let url = URL(string: urlString)!
+        let req = AF.request(url)
+        req.validate().responseData { response in
+            switch response.result {
+            case .success(let data):
+                cell.musicImageView.image = UIImage(data: data)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        cell.artistNameLabel.text = urlMusicData[indexPath.row].artistName
+        cell.musicNameLabel.text = urlMusicData[indexPath.row].trackName
+        
+        return cell
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        print("스코프가 눌렸음")
+        
+        guard let text = searchBar.text else { return }
+        url = "https://itunes.apple.com/search?media=music&entity=song&term=" + text
+        getData()
+    }
+}
