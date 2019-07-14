72b7476ba9ac3599c89ca3436506c8b6dc66cf95
chunsu Kim <chunsu0816@gmail.com>
2019-07-14 21:07:39 +0900

    initial commit for Google MapView API Example

---
+++ MapViewGoogleAPI/MapViewGoogleAPI/Common.swift
@@ -0,0 +1,11 @@
+//
+//  Common.swift
+//  MapViewGoogleAPI
+//
+//  Created by Chunsu Kim on 10/07/2019.
+//  Copyright © 2019 Chunsu Kim. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "Your API Key"
---
+++ MapViewGoogleAPI/MapViewGoogleAPI/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MapViewGoogleAPI
+//
+//  Created by Chunsu Kim on 10/07/2019.
+//  Copyright © 2019 Chunsu Kim. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+
+}
