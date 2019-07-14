ab480ec71934d7a070d1ab503112186b8bc06a7b
chunsu Kim <chunsu0816@gmail.com>
2019-07-14 21:12:22 +0900

    conf, feat: Organized folder and created extension
    1. created extension for TextField
    2. organized folders for MVC pattern which were Controllers, View, and Extension

---
+++ MapViewGoogleAPI/MapViewGoogleAPI/Extension/TextFieldExtension.swift
@@ -0,0 +1,31 @@
+//
+//  TextFieldExtension.swift
+//  MapViewGoogleAPI
+//
+//  Created by Chunsu Kim on 14/07/2019.
+//  Copyright © 2019 Chunsu Kim. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        leftView = paddingView
+        leftViewMode = .always
+    }
+    
+    func setRightPaddingPoints(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        rightView = paddingView
+        rightViewMode = .always
+    }
+    
+    func returnIntTextValue() -> Int? {
+        if let text = self.text {
+            return Int(text)
+        }
+        return nil
+    }
+}
