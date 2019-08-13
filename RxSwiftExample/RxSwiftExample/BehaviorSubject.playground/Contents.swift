ecf0299e8bcb09af1398d8008ca2c0863135f2cb
chunsu Kim <chunsu0816@gmail.com>
2019-08-13 19:30:10 +0900

    conf: RxSwiftExample

---
+++ RxSwiftExample/RxSwiftExample/BehaviorSubject.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+import RxSwift
+
+let disposeBag = DisposeBag()
+let numbers$ = BehaviorSubject(value: -1)
+
+numbers$
+    .subscribe(onNext: { print("Observer 1: \($0)") })
+    .disposed(by: disposeBag)
+
+numbers$.onNext(0)
+numbers$.onNext(1)
+
+numbers$.subscribe(onNext: { print("Observer 2: \($0)") })
+    .disposed(by: disposeBag)
+
+numbers$.onNext(2)
+numbers$.onNext(3)
+numbers$.onCompleted()
+
+
+/*
+ Observer 1: -1
+ Observer 1: 0
+ Observer 1: 1
+ Observer 2: 1
+ Observer 1: 2
+ Observer 2: 2
+ Observer 1: 3
+ Observer 2: 3
+ */
+
+
