3119c454ba3843bd8f48674e2e28d49a90ba41be
chunsu Kim <chunsu0816@gmail.com>
2020-03-26 17:37:01 +0900

    conf: Sign Up User Interface

--- InstagramCopy/InstagramCopy/AppDelegate.swift
+++ InstagramCopy/InstagramCopy/AppDelegate.swift
@@ -16,11 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window? = UIWindow(frame: UIScreen.main.bounds)
-        
-        window?.backgroundColor = .white
-        window?.rootViewController = LoginVC()
-        window?.makeKeyAndVisible()
+        window? = UIWindow()
+        window?.rootViewController = UINavigationController(rootViewController: LoginVC())
         
         return true
     }
--- InstagramCopy/InstagramCopy/Controller/LoginVC.swift
+++ InstagramCopy/InstagramCopy/Controller/LoginVC.swift
@@ -22,7 +22,7 @@ class LoginVC: UIViewController {
         return view
     }()
     
-    let emailtextField: UITextField = {
+    let emailTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Email"
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
@@ -52,8 +52,10 @@ class LoginVC: UIViewController {
     let dontHaveAccountButton: UIButton = {
         let button = UIButton(type: .system)
         
-        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?   ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor(red: 17/255, green: 154/255, blue: 237/255, alpha: 1)]))
+        
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
         
         button.setAttributedTitle(attributedTitle, for: .normal)
         
@@ -66,6 +68,9 @@ class LoginVC: UIViewController {
         // background color
         view.backgroundColor = .white
         
+        // hide navigation bar
+        navigationController?.navigationBar.isHidden = true
+        
         view.addSubview(logoContainerView)
         logoContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 150)
         
@@ -75,9 +80,17 @@ class LoginVC: UIViewController {
         dontHaveAccountButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
     }
     
+    @objc func handleShowSignUp() {
+        let signUpVC = SignUpVC()
+        navigationController?.pushViewController(signUpVC, animated: true)
+        
+        print("Handle show sign up here..")
+        
+    }
+    
     func configureViewComponents() {
         
-        let stackView = UIStackView(arrangedSubviews: [emailtextField, passwordtextField, loginButton])
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordtextField, loginButton])
         
         stackView.axis = .vertical
         stackView.spacing = 10
